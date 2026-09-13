56d5f203a720d39f5c79a1084cc6e2a0fa698059
totalmotors28 <totalmotors28@gmail.com>
2026-09-13 15:59:21 +0500

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() => runApp(const AydymCalamApp());
+
+class AydymCalamApp extends StatelessWidget {
+  const AydymCalamApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aýdym Çalar',
+      debugShowCheckedModeBanner: false,
+      // Только туркменская локаль
+      locale: const Locale('tk', 'TM'),
+      supportedLocales: const [Locale('tk', 'TM')],
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF7C4DFF),
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
